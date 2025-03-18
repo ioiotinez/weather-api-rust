29e4d318005b64be5cc966f7b42dc65c52940317
ioiotinez <ioiotinez@gmail.com>
2025-03-18 09:06:49 -0300

    Add reqwest and env var

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+/* I want to create an API using actix  */
+
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use std::env;
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/weather")]
+async fn invoke_weather_api() -> impl Responder {
+    let api_key = env::var("API_KEY").expect("API_KEY must be set");
+    let city = "Montevideo";
+    let path = format!("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{}?unitGroup=metric&include=days&key={}&contentType=json", city, api_key);
+    let response = reqwest::get(path).await.unwrap().text().await.unwrap();
+
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body(response)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok(); // Load the .env file
+    HttpServer::new(|| App::new().service(index).service(invoke_weather_api))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
